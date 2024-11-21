e2f4e4585a0752a10fdf3e3e0f3c45ef2e4ae755
Vajkyy <vajki2014@gmail.com>
2024-11-21 14:02:47 +0100

    kezdes

---
+++ src/jatek/kopapirollo/Kopapirollo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jatek.kopapirollo;
+
+/**
+ *
+ * @author HómanVajkGábor(Szf_N
+ */
+public class Kopapirollo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
