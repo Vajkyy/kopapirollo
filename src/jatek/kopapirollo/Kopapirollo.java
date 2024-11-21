ec94a1c91922274dd7850c2d7ba59b1cd664e757
Vajkyy <vajki2014@gmail.com>
2024-11-21 14:46:07 +0100

    robot valaszai

--- src/jatek/kopapirollo/Kopapirollo.java
+++ src/jatek/kopapirollo/Kopapirollo.java
@@ -7,10 +7,25 @@ public class Kopapirollo {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Válassz: (kő, papír, olló)");
+        System.out.print("Válassz: (kő, papír, olló): ");
+        
         String valasz = sc.nextLine();
+        
         int gepValasztSzam = (int) (Math.random() * 3);
         String gepValasztas = "";
+        
+        if (gepValasztSzam == 0){
+            gepValasztas = "kő";
+        } else if (gepValasztSzam == 1){
+            gepValasztas = "papír";
+        } else if (gepValasztSzam == 2){
+            gepValasztas = "olló";
+        }
+        System.out.println("A gép választása: " + gepValasztas);
+        
+        if (valasz.equals(gepValasztas)){
+            System.out.println("Döntetlen");
+        }
     }
     
 }
